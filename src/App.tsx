5d4143d4eccec8a0acf581455505398eafe7445b
hwahee <pjh5272@metalab.co.kr>
2022-05-09 12:51:29 +0900

    오브젝트에 카메라 설치

--- src/App.tsx
+++ src/App.tsx
@@ -29,14 +29,11 @@ const Player = (props: { position: number[], speed?: number }) => {
 
 		const posnow = ref.current.position
 		ref.current.position.set(posnow.x + moveVec.x * delta, posnow.y, posnow.z + moveVec.z * delta)
-
 	})
-
-	useEffect(() => {
-	}, [])
 
 	return <>
 		<Sphere ref={ref}>
+			<PerspectiveCamera makeDefault position={[0, 2, 0]} rotation={[0, Math.PI, 0]}/>
 			<Box scale={[4, 0.5, 0.5]} position={[2, 0, 0]}>
 				<meshPhongMaterial color={'red'} />
 			</Box>
@@ -51,22 +48,36 @@ const Player = (props: { position: number[], speed?: number }) => {
 	</>
 }
 
-const Camera = () => {
-	const ref = useRef<PerspectiveCameraImpl>()
-	return <PerspectiveCamera ref={ref} />
+// const Camera = () => {
+// 	const ref = useRef<PerspectiveCameraImpl>()
+// 	return <PerspectiveCamera ref={ref} />
+// }
+const AxisIndicator = () => {
+	return <>
+		<Box scale={[4, 1 / 16, 1 / 16]} position={[2, 0, 0]}>
+			<meshPhongMaterial color={'red'} />
+		</Box>
+		<Box scale={[1 / 16, 4, 1 / 16]} position={[0, 2, 0]}>
+			<meshPhongMaterial color={'blue'} />
+		</Box>
+		<Box scale={[1 / 16, 1 / 16, 4]} position={[0, 0, 2]}>
+			<meshPhongMaterial color={'green'} />
+		</Box>
+	</>
 }
 
 function App() {
 	return (
 		<div id='canvas'>
 			<Canvas>
-				<OrbitControls />
+				{/* <OrbitControls /> */}
 				<ambientLight />
 				<Player position={[0, 1, 0]} speed={4} />
 				<pointLight position={[10, 10, 10]} />
 				<Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]}>
 					<meshPhongMaterial color={'yellowgreen'} />
 				</Plane>
+				<AxisIndicator />
 			</Canvas>
 		</div>
 	);
