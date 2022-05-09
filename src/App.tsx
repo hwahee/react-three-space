7877538a26627ff7a8ae426a708c4b9e72cf883d
hwahee <pjh5272@metalab.co.kr>
2022-05-09 12:20:50 +0900

    8방향 이동, 회전 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 import * as THREE from 'three'
 import { useStore } from './store';
-import { Canvas} from '@react-three/fiber';
-import { Box, OrbitControls, Plane, Sphere } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Box, OrbitControls, PerspectiveCamera, Plane, Sphere } from '@react-three/drei'
 import { useKeyboard } from './useKeyboard';
+import { PerspectiveCamera as PerspectiveCameraImpl, Vector3 } from 'three';
 
-
-const Player = (props: { position: number[] }) => {
-	const { position: pos } = props
-
+const Player = (props: { position: number[], speed?: number }) => {
+	const { position, speed = 1 } = props
 	const ref = useRef<THREE.Mesh>(null!)
-	const [vel, setVel] = useState([0, 0, 0])
 	const keyStat = useKeyboard()
 
 	useEffect(() => {
-		ref.current.position.set(pos[0], pos[1], pos[2])
+		ref.current.position.set(position[0], position[1], position[2])
 	}, [ref.current])
 
 	useEffect(() => { }, [keyStat])
+	useFrame((state, delta) => {
+		const frontVec = (keyStat['w'] ? 1 : 0) + (keyStat['s'] ? -1 : 0)
+		const sideVec = (keyStat['a'] ? 1 : 0) + (keyStat['d'] ? -1 : 0)
+		const rotDelta = (keyStat['q'] ? 1 : 0) + (keyStat['e'] ? -1 : 0)
+
+		const posnow = ref.current.position
+		ref.current.position.set(posnow.x + sideVec * delta * speed, posnow.y, posnow.z + frontVec * delta * speed)
+
+		const rot=ref.current.rotation
+		ref.current.rotation.set(rot.x, rot.y+rotDelta * delta, rot.z)
+	})
+
+	useEffect(() => {
+	}, [])
 
 	return <>
 		<Sphere ref={ref}>
+			<Box scale={[4, 0.5, 0.5]} position={[2, 0, 0]}>
+				<meshPhongMaterial color={'red'} />
+			</Box>
+			<Box scale={[0.5, 4, 0.5]} position={[0, 2, 0]}>
+				<meshPhongMaterial color={'blue'} />
+			</Box>
+			<Box scale={[0.5, 0.5, 4]} position={[0, 0, 2]}>
+				<meshPhongMaterial color={'green'} />
+			</Box>
 			<meshPhongMaterial color={'hotpink'} />
 		</Sphere>
 	</>
+}
+
+const Camera = () => {
+	const ref = useRef<PerspectiveCameraImpl>()
+	return <PerspectiveCamera ref={ref} />
 }
 
 function App() {
@@ -34,7 +60,7 @@ function App() {
 			<Canvas>
 				<OrbitControls />
 				<ambientLight />
-				<Player position={[0, 1, 0]} />
+				<Player position={[0, 1, 0]} speed={4} />
 				<pointLight position={[10, 10, 10]} />
 				<Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]}>
 					<meshPhongMaterial color={'yellowgreen'} />
